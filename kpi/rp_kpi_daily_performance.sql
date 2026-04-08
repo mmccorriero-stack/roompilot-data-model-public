bc9e3b080a336b05522c44ccfdcbf1dbb19d1e04
MarCorri <mmc.corriero@gmail.com>
2026-04-08 12:41:05 +0200

    Update and rename kpi_daily_performance.sql to rp_kpi_daily_performance.sql

--- kpi/rp_kpi_daily_performance.sql
+++ kpi/rp_kpi_daily_performance.sql
@@ -1,12 +1,11 @@
-DROP TABLE IF EXISTS kpi_daily_performance;
+DROP TABLE IF EXISTS rp_kpi_daily_performance;
 
-CREATE TABLE kpi_daily_performance AS
+CREATE TABLE rp_kpi_daily_performance AS
 SELECT
     i.inventory_date AS stay_date,
     i.inventory_date_key AS stay_date_key,
     i.hotel_id,
     i.hotel_name,
-
     i.total_units,
     i.ooo_units,
     i.blocked_units,
@@ -15,29 +14,29 @@ SELECT
     i.available_units,
     i.is_open,
 
-    COALESCE(COUNT(rn.stay_date), 0) AS room_nights_sold,
-    COALESCE(ROUND(SUM(rn.night_revenue), 2), 0) AS room_revenue,
+    COALESCE(SUM(rn.rooms_booked), 0) AS room_nights_sold,
+    COALESCE(ROUND(SUM(rn.night_revenue_total), 2), 0) AS room_revenue,
 
     CASE
-        WHEN COUNT(rn.stay_date) > 0
-            THEN ROUND(SUM(rn.night_revenue) / COUNT(rn.stay_date), 2)
+        WHEN COALESCE(SUM(rn.rooms_booked), 0) > 0
+            THEN ROUND(SUM(rn.night_revenue_total) / SUM(rn.rooms_booked), 2)
         ELSE 0
     END AS adr,
 
     CASE
         WHEN i.available_units > 0
-            THEN ROUND(COUNT(rn.stay_date) * 100.0 / i.available_units, 2)
+            THEN ROUND(COALESCE(SUM(rn.rooms_booked), 0) * 100.0 / i.available_units, 2)
         ELSE 0
     END AS occupancy_pct,
 
     CASE
         WHEN i.available_units > 0
-            THEN ROUND(COALESCE(SUM(rn.night_revenue), 0) / i.available_units, 2)
+            THEN ROUND(COALESCE(SUM(rn.night_revenue_total), 0) / i.available_units, 2)
         ELSE 0
     END AS revpar
 
-FROM fact_inventory_daily i
-LEFT JOIN fact_room_nights rn
+FROM rp_fact_inventory_daily i
+LEFT JOIN rp_fact_room_nights rn
     ON i.hotel_id = rn.hotel_id
    AND i.inventory_date = rn.stay_date
    AND rn.is_cancelled = 0
