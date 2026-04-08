3ecdf146cdbde3527ad84ae9bf3379ae740bd773
MarCorri <mmc.corriero@gmail.com>
2026-04-08 12:40:12 +0200

    Update rp_fact_inventory_daily.sql

--- fact/rp_fact_inventory_daily.sql
+++ fact/rp_fact_inventory_daily.sql
@@ -1,31 +1,107 @@
-DROP TABLE IF EXISTS rp_fact_inventory_daily;
+DROP TABLE IF EXISTS rp_fact_room_nights;
 
-CREATE TABLE rp_fact_inventory_daily AS
+CREATE TABLE rp_fact_room_nights AS
+WITH RECURSIVE room_nights AS (
+    SELECT
+        reservation_id,
+        hotel_id,
+        source_system,
+        booking_date,
+        booking_date_key,
+        checkin_date,
+        checkout_date,
+        reservation_status,
+        channel_raw,
+        segment_raw,
+        rate_plan,
+        board_code,
+        policy_name,
+        adults,
+        teens,
+        children,
+        guest_count,
+        rooms_booked,
+        stay_nights,
+        room_revenue,
+        total_amount,
+        paid_amount,
+        is_cancelled,
+        checkin_date AS stay_date
+    FROM rp_fact_reservations
+    WHERE checkin_date IS NOT NULL
+      AND checkout_date IS NOT NULL
+      AND julianday(checkout_date) > julianday(checkin_date)
+
+    UNION ALL
+
+    SELECT
+        reservation_id,
+        hotel_id,
+        source_system,
+        booking_date,
+        booking_date_key,
+        checkin_date,
+        checkout_date,
+        reservation_status,
+        channel_raw,
+        segment_raw,
+        rate_plan,
+        board_code,
+        policy_name,
+        adults,
+        teens,
+        children,
+        guest_count,
+        rooms_booked,
+        stay_nights,
+        room_revenue,
+        total_amount,
+        paid_amount,
+        is_cancelled,
+        date(stay_date, '+1 day')
+    FROM room_nights
+    WHERE date(stay_date, '+1 day') < checkout_date
+)
 SELECT
-    d.full_date AS inventory_date,
-    d.date_key AS inventory_date_key,
-    h.hotel_id,
-    h.hotel_name,
-    h.total_units,
-
-    0 AS ooo_units,
-    0 AS blocked_units,
-    0 AS closed_units,
-    0 AS maintenance_units,
-
-    h.total_units AS available_units,
-
+    reservation_id,
+    hotel_id,
+    source_system,
+    booking_date,
+    booking_date_key,
+    checkin_date,
+    checkout_date,
+    stay_date,
+    CAST(strftime('%Y%m%d', stay_date) AS INTEGER) AS stay_date_key,
+    reservation_status,
+    channel_raw,
+    segment_raw,
+    rate_plan,
+    board_code,
+    policy_name,
+    adults,
+    teens,
+    children,
+    guest_count,
+    rooms_booked,
+    stay_nights,
+    ROUND(room_revenue, 2) AS room_revenue,
+    ROUND(total_amount, 2) AS total_amount,
+    ROUND(paid_amount, 2) AS paid_amount,
     CASE
-        WHEN h.total_units > 0 THEN 1
+        WHEN stay_nights > 0 THEN ROUND(room_revenue / stay_nights, 2)
         ELSE 0
-    END AS is_open,
-
-    NULL AS notes
-
-FROM dim_date d
-CROSS JOIN dim_hotel h
-WHERE h.is_active = 1;
-
-FROM dim_date d
-CROSS JOIN dim_hotel h
-WHERE h.is_active = 1;
+    END AS night_revenue_total,
+    CASE
+        WHEN stay_nights > 0 AND rooms_booked > 0 THEN ROUND(room_revenue / stay_nights / rooms_booked, 2)
+        ELSE 0
+    END AS night_revenue_per_unit,
+    CASE
+        WHEN stay_date = checkin_date THEN 1
+        ELSE 0
+    END AS is_arrival_night,
+    CASE
+        WHEN date(stay_date, '+1 day') = checkout_date THEN 1
+        ELSE 0
+    END AS is_last_night,
+    is_cancelled
+FROM room_nights;
